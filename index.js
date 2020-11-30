28331c5900582c1ac953b403412bc449f6bcef25
tahmid17 <timambinsaif462@gmail.com>
2020-11-30 10:35:32 +0600

    asdasd

---
+++ index.js
@@ -0,0 +1,26 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDoFUdL815RsJYK6aNS9tDle-RnpvHFm8k",
+    authDomain: "tradefeed-d399f.firebaseapp.com",
+    databaseURL: "https://tradefeed-d399f.firebaseio.com",
+    projectId: "tradefeed-d399f",
+    storageBucket: "tradefeed-d399f.appspot.com",
+    messagingSenderId: "934728586568",
+    appId: "1:934728586568:web:12aad6ec7693bb2e9c4f7d",
+    measurementId: "G-T1S5JDMGMM"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+   const auth=firebase.auth;
+function login1(){
+    var userEmail= document.getElementById("Email").value;
+    var userPassword= document.getElementById("password").value;
+    /*window.alert(userEmail+userPassword);*/
+   const promise=auth.createUserWithEmailAndPassword(userEmail,userPassword);
+  window.alert(auth.promise.value);
+   
+    
+}
