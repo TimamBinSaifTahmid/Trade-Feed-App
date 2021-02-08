2bf9d1b1271a7b2444f6195b7991339deede66aa
tahmid17 <timambinsaif462@gmail.com>
2021-02-08 15:29:22 +0600

    product list modification

--- product_list.js
+++ product_list.js
@@ -48,3 +48,54 @@ var firebaseConfig = {
       }
     });
   }
+  
+  function retrive_ProductSeller_Info(){
+    //var email_id=document.getElementById(email).value;
+    //window.alert(email);
+    var temp=0;
+    var uid=localStorage.getItem("u_id");
+    var seller_id_list=firebase.database().ref('ProductSellerlist/');
+    var retrive_seller_id = firebase.database().ref('User/');
+    var retrive_seller_information = firebase.database().ref('UserProduct/');
+    var retrive_Productname =firebase.database().ref("Product/")
+    seller_id_list.on('value',function(snapshot4){
+    retrive_seller_id.on('value',function(snapshot)
+    {
+      retrive_seller_information.on('value',function(snapshot1){
+        retrive_Productname.on('value',function(snapshot2){
+         var product_Seller_Count=snapshot4.numChildren();
+          for(var j=1;j<=product_Seller_Count;i++){
+          var product_seller_id=snapshot4.child(j).val();
+           seller_Product_Count= snapshot1.child(product_seller_id).numChildren();
+           var product_id=localStorage.getItem( "product_id");
+         product_Name= snapshot2.child(product_id).val();
+         for(var i=1;i<=seller_Product_Count;i++){
+          retreived_Product_Name =snapshot1.child(product_seller_id).child(i).val().ProductName;
+          if(retreived_Product_Name= product_Name){
+
+            ++temp;
+            
+           
+            temp+'price';
+            temp+'Seller_rating';
+           document.getElementById(temp+'seller_email').innerHTML=snapshot.child(product_seller_id).val().EmailAddress;
+           document.getElementById (temp+'Available_amount').innerHTML=snapshot1.child(product_seller_id).child(i).val().Amount;
+           document.getElementById(temp+'price').innerHTML=snapshot1.child(product_seller_id).child(i).val().Price;
+           document.getElementById(temp+'Seller_rating').innerHTML=0;
+          }
+          
+        }
+          
+      } 
+
+     });   
+
+   });
+
+  });
+
+ });
+    
+}
+      
+  
