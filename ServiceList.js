d8ed8ff95f00d1ceb7aa5d1b708b63178db3d94b
EushaMashfi <eushamashfi@gmail.com>
2021-02-13 01:07:55 +0600

    bug fix

--- ServiceList.js
+++ ServiceList.js
@@ -10,161 +10,171 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var serviceReceiverAddress= new Array(2).fill(0);
-var serviceProviderAddress= new Array(6).fill(0);
-var distances= new Array(6).fill(0);
-var providernames= new Array(6).fill(0);
-var wage= new Array(6).fill(0);
-var skill= new Array(6).fill(0);
-var ratings= new Array(6).fill(0);
-  //var order_tracker=new Array(100).fill(0);
+var serviceReceiverAddress = new Array(2).fill(0);
+var serviceProviderAddress = new Array(6).fill(0);
+var distances = new Array(6).fill(0);
+var providernames = new Array(6).fill(0);
+var wage = new Array(6).fill(0);
+var skill = new Array(6).fill(0);
+var ratings = new Array(6).fill(0);
+//var order_tracker=new Array(100).fill(0);
 
-window.onload=function(){
+window.onload = function () {
   retrive_ProductSeller_Info();
 }
 
 
-function seller_email(id){
-  //window.alert("seller email id");
-  var btn_id=parseInt(id);
-  var sell_id=localStorage.getItem(btn_id);
-  var dstr='d'+parseInt(id);
-  var distnce=localStorage.getItem(dstr);
-  localStorage.removeItem("s_id");
-  localStorage.setItem("s_id",sell_id);
-  localStorage.removeItem("distance");
-  localStorage.setItem("distance",distnce);
-  window.location.href="service_order.html";
+function seller_email(id) {
+  window.alert(id);
+  var btn_id = parseInt(id);
+  var sell_id = localStorage.getItem(btn_id);
+  var dstr = 'd' + parseInt(id);
+  var distnce = localStorage.getItem(dstr);
+  localStorage.removeItem('s_id');
+  localStorage.setItem('s_id', sell_id);
+  localStorage.removeItem('distance');
+  localStorage.setItem('distance', distnce);
+  window.location.href = "service_order.html";
 }
- 
 
 
 
 
-function retrive_ProductSeller_Info(){
+
+function retrive_ProductSeller_Info() {
+
   //window.alert("function dhukse");
   //var email_id=document.getElementById(email).value;
   //window.alert(email);
-  var temp=0;
-  var uid=localStorage.getItem("u_id");
-  var provider_Id_List=firebase.database().ref('serviceProviderlist/');
+  var temp = 0;
+  var uid = localStorage.getItem("u_id");
+  var provider_Id_List = firebase.database().ref('serviceProviderlist/');
   var retrive_provider_id = firebase.database().ref('User/');
   var retrive_provider_information = firebase.database().ref('UserService/');
-  var retrive_ServiceName =firebase.database().ref("Service/");
-  provider_Id_List.on('value',function(snapshot4){
-  retrive_provider_id.on('value',function(snapshot)
-  {
-    retrive_provider_information.on('value',function(snapshot1){
-      retrive_ServiceName.on('value',function(snapshot2){
-      
-    
-       var product_Seller_Count=snapshot4.numChildren();
-      // window.alert("for er age");
-        for(var j=1;j<=product_Seller_Count;j++){
-       //   window.alert("for er pore");  
-        var product_seller_id=snapshot4.child(j).val();
-        //window.alert(product_seller_id);
-         seller_Product_Count= snapshot1.child(product_seller_id).numChildren();
-         var service_id=localStorage.getItem("Service_id");
-        
-       service_Name= snapshot2.child(service_id).val();
-      // window.alert(service_id+ ' '+service_Name);
-      for(var i=1;i<=seller_Product_Count;i++){
-      // window.alert(product_seller_id);
-      const service_name1 =snapshot1.child(product_seller_id).child(i).val().ServiceName;
-      // window.alert(service_Name+' '+service_name1);
-        if(service_name1==service_Name){
-         
-          ++temp;
-          //window.alert(retreived_Product_Name);
-          
-            localStorage.removeItem(temp);
-            localStorage.setItem(temp,product_seller_id);
-          
-         
-         
-           serviceReceiverAddress[1]=snapshot.child(uid).val().Address;   
-           //window.alert(serviceReceiverAddress[1]);   
-          serviceProviderAddress[temp]=snapshot.child(product_seller_id).val().Address;
-          window.alert(serviceProviderAddress[temp]);
-         providernames[temp]=snapshot.child(product_seller_id).val().Name;
-           wage[temp]=snapshot1.child(product_seller_id).child(i).val().Wage;
-         // skill[temp]=snapshot1.child(product_seller_id).child(i).val().Skill;
-          
-          sortDistance();
-          
-         //document.getElementById(temp+'seller_email').innerHTML=snapshot.child(product_seller_id).val().EmailAddress;
-         ///document.getElementById (temp+'Available_amount').innerHTML=snapshot1.child(product_seller_id).child(i).val().Amount;
-         //document.getElementById(temp+'price').innerHTML=snapshot1.child(product_seller_id).child(i).val().Price;
-         //document.getElementById(temp+'Seller_rating').innerHTML=0;
-        }
-        
-      }
-        
-    } 
+  var retrive_ServiceName = firebase.database().ref("Service/");
+  provider_Id_List.on('value', function (snapshot4) {
+    retrive_provider_id.on('value', function (snapshot) {
+      retrive_provider_information.on('value', function (snapshot1) {
+        retrive_ServiceName.on('value', function (snapshot2) {
 
-   });   
 
- });
+          var product_Seller_Count = snapshot4.numChildren();
+          // window.alert("for er age");
+          for (var j = 1; j <= product_Seller_Count; j++) {
+            window.alert("for er pore");
+            var product_seller_id = snapshot4.child(j).val();
+            //window.alert(product_seller_id);
+            seller_Product_Count = snapshot1.child(product_seller_id).numChildren();
+            var service_id = localStorage.getItem("Service_id");
 
-});
+            service_Name = snapshot2.child(service_id).val();
+            // window.alert(service_id+ ' '+service_Name);
+            for (var i = 1; i <= seller_Product_Count; i++) {
+              //     window.alert(product_seller_id);
+              const service_name1 = snapshot1.child(product_seller_id).child(i).val().ServiceName;
+              // window.alert(service_Name+' '+service_name1);
+              if (service_name1 == service_Name) {
 
-});
-  
+                ++temp;
+                //window.alert(retreived_Product_Name);
+
+                localStorage.removeItem(temp);
+                localStorage.setItem(temp, product_seller_id);
+
+
+
+                serviceReceiverAddress[1] = snapshot.child(uid).val().Address;
+                //  window.alert(serviceReceiverAddress[1]);   
+                serviceProviderAddress[temp] = snapshot.child(product_seller_id).val().Address;
+                //  window.alert(serviceProviderAddress[temp]);
+                providernames[temp] = snapshot.child(product_seller_id).val().Name;
+
+                wage[temp] = snapshot1.child(product_seller_id).child(i).val().Wage;
+                // skill[temp]=snapshot1.child(product_seller_id).child(i).val().Skill;
+
+                // window.alert(providernames[temp]+' '+wage[temp] );
+
+                //document.getElementById(temp+'seller_email').innerHTML=snapshot.child(product_seller_id).val().EmailAddress;
+                ///document.getElementById (temp+'Available_amount').innerHTML=snapshot1.child(product_seller_id).child(i).val().Amount;
+                //document.getElementById(temp+'price').innerHTML=snapshot1.child(product_seller_id).child(i).val().Price;
+                //document.getElementById(temp+'Seller_rating').innerHTML=0;
+              }
+
+            }
+            if (j == product_Seller_Count) {
+              //    window.alert('eibar kaj hbe');
+              sortDistance(temp);
+            }
+          }
+
+
+        });
+
+      });
+
+    });
+
+  });
+
 }
-    
-function sortDistance(){
 
-var retrieve_distance =firebase.database().ref("Distance_From_center/");
-retrieve_distance.on('value',function(snapshot){
-   for(var i=1;i<=5;i++){
-     distances[i]=snapshot.child(serviceProviderAddress[i]).val()+snapshot.child(serviceReceiverAddress[1]).val();
-     //window.alert(sellerAddress[i]));
-    // window.alert(snapshot.child(buyerAddress[1]).val());
-    if(i==5){
-      sort();
-    } 
-    } 
-    
-});
+function sortDistance(temp) {
 
-}  
-function sort(){
-
-for(var i=1;i<=2;i++){
-  for(var j=1;j<2;j++){
-    if(distances[j]>distances[j+1]){
-      var temp=distances[j];
-      var temp2=providernames[j];
-      var temp3=amounts[j];
-      var temp4=prices[j];
-      distances[j]=distances[j+1];
-      providernames[j]=providernames[j+1];
-      amounts[j]=amounts[j+1];
-      prices[j]=prices[j+1];
-      distances[j+1]=temp;
-      providernames[j+1]=temp2;
-      amounts[j+1]=temp3;
-      prices[j+1]=temp4;
+  var retrieve_distance = firebase.database().ref("Distance_From_center/");
+  retrieve_distance.on('value', function (snapshot) {
+    for (var k = 1; k <= temp; k++) {
+      distances[k] = snapshot.child(serviceProviderAddress[k]).val() + snapshot.child(serviceReceiverAddress[1]).val();
+      //window.alert(sellerAddress[i]));
+      // window.alert(snapshot.child(buyerAddress[1]).val());
+      if (k == temp) {
+        sort(temp);
+      }
     }
-  }if(i==2){
-  set_val();
+
+  });
+
+}
+function sort(temp) {
+
+  for (var l = 1; l <= temp; l++) {
+    for (var m = 1; m < temp; m++) {
+      if (distances[m] > distances[m + 1]) {
+        var temp = distances[m];
+        var temp2 = providernames[m];
+        var temp3 = wage[m];
+        // var temp4 = prices[m];
+        distances[m] = distances[m + 1];
+        providernames[m] = providernames[m + 1];
+        wage[m] = wage[m + 1];
+        // prices[m] = prices[m + 1];
+        distances[m + 1] = temp;
+        providernames[m + 1] = temp2;
+        amounts[m + 1] = temp3;
+        // prices[m + 1] = temp4;
+      }
+    } if (l == temp) {
+      set_val(temp);
+    }
   }
 }
-}
-function set_val(){
+function set_val(temp) {
+  // window.alert(temp);
+  for (var p = 1; p <= temp; p++) {
 
-for(var i=1;i<=2;i++){
-   
-  document.getElementById(i+'provider_Name').innerHTML=providernames[i];
-  document.getElementById (i+'provider_Wages').innerHTML=wage[i];
- // document.getElementById(i+'price').innerHTML=prices[i];
-  document.getElementById(i+'distance').innerHTML=distances[i];
-  document.getElementById(i+'Seller_rating').innerHTML=0;
-  //window.alert(amounts[i]);
-  var dstr1='d'+i;
-  localStorage.removeItem(dstr1);
-  localStorage.setItem(dstr1,distances[i]);
-  
-}
+    if (p == temp) {
+      window.alert('SUCCESSFUL');
+    }
+
+    //window.alert(providernames[p]+' '+ wage[p]+' '+distances[p]);
+    document.getElementById(p + 'provider_Name').innerHTML = providernames[p];
+    document.getElementById(p + 'provider_Wages').innerHTML = wage[p];
+    // document.getElementById(i+'price').innerHTML=prices[i];
+    document.getElementById(p + 'distance').innerHTML = distances[p];
+    document.getElementById(p + 'provider_Rating').innerHTML = 0;
+    //window.alert(amounts[i]);
+    var dstr1 = 'd' + p;
+    localStorage.removeItem(dstr1);
+    localStorage.setItem(dstr1, distances[p]);
+
+  }
 }
--- service_order.js
+++ service_order.js
@@ -11,126 +11,127 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-window.onload=function(){
- seller_profile();
+window.onload = function () {
+  seller_profile();
 }
 
 
-function showpopup(){
-document.querySelector(".popup").style.display="flex";
+function showpopup() {
+  document.querySelector(".popup").style.display = "flex";
 }
-function closepopup(){
-  document.querySelector(".popup").style.display="none";
-  }
+function closepopup() {
+  document.querySelector(".popup").style.display = "none";
+}
 
 
 
 
-function seller_profile(){
-  var sid=localStorage.getItem("s_id");
-  var uid=localStorage.getItem("u_id");
-  const scvid=localStorage.getItem("Service_id");
- // window.alert(scvid);
-  var oid=parseInt(sid)+parseInt(uid);
+function seller_profile() {
+  var sid = localStorage.getItem('s_id');
+  var uid = localStorage.getItem("u_id");
+  const scvid = localStorage.getItem("Service_id");
+  window.alert(sid + ' ' + uid);
+  var oid = parseInt(sid) + parseInt(uid);
   var amount;
   var price;
   var sold;
   var rating;
   var firepro1 = firebase.database().ref('User/');
-  var firepro2 = firebase.database().ref('UserService/'+sid);
+  var firepro2 = firebase.database().ref('UserService/' + sid);
   // window.alert(sid);
-  var firepro3=firebase.database().ref('Seller_rating/');
+  var firepro3 = firebase.database().ref('Seller_rating/');
   var firepro4 = firebase.database().ref('Service/');
-  
-  firepro1.on('value',function(snapshot)
-    {
-      firepro2.on('value',function(snapshot2){
-        firepro3.on('value',function(snapshot3){
-          firepro4.on('value',function(snapshot4){
-          //    window,alert("ref e dhukse");
-          const childcnt1= snapshot2.numChildren();
-         // window.alert(childcnt1);
-          for(var i=1;i<=childcnt1;i++){
-        //    window.alert("for e dhukse ree");
-            var scid=snapshot2.child(i).val().ServiceName;
-            var sname=snapshot4.child(scvid).val();
-        //    window.alert(scid+' '+sname);
-            if(sname==scid){
-                  window.alert("2nd ifor");
-                  document.getElementById('name').innerHTML=snapshot.child(sid).val().Name;
-                  document.getElementById('address').innerHTML=snapshot.child(sid).val().Address;
-                  document.getElementById('email').innerHTML=snapshot.child(sid).val().EmailAddress;
-                  document.getElementById('wage').innerHTML=snapshot2.child(i).val().Wage;
-                  document.getElementById('skill').innerHTML=snapshot2.child(i).val().Skill;
-                 document.getElementById('Experience').innerHTML=snapshot2.child(i).val().Experience;
-                 document.getElementById('rate').innerHTML=snapshot3.child(sid).val().Rate;
-                 document.getElementById('phn_no').innerHTML = snapshot.child(sid).val().ContactNo;
+
+  firepro1.on('value', function (snapshot) {
+    firepro2.on('value', function (snapshot2) {
+      firepro3.on('value', function (snapshot3) {
+        firepro4.on('value', function (snapshot4) {
+          //   window,alert("ref e dhukse");
+          const childcnt1 = snapshot2.numChildren();
+          window.alert(childcnt1);
+          for (var i = 1; i <= childcnt1; i++) {
+            //    window.alert("for e dhukse ree");
+            var scid = snapshot2.child(i).val().ServiceName;
+            var sname = snapshot4.child(scvid).val();
+            window.alert(scid + ' ' + sname);
+            if (sname == scid) {
+              window.alert("2nd ifor");
+              document.getElementById('name').innerHTML = snapshot.child(sid).val().Name;
+              document.getElementById('address').innerHTML = snapshot.child(sid).val().Address;
+              document.getElementById('email').innerHTML = snapshot.child(sid).val().EmailAddress;
+              document.getElementById('wage').innerHTML = snapshot2.child(i).val().Wage;
+              document.getElementById('skill').innerHTML = snapshot2.child(i).val().Skill;
+              document.getElementById('Experience').innerHTML = snapshot2.child(i).val().Experience;
+              document.getElementById('phn_no').innerHTML = snapshot.child(sid).val().ContactNo;
+              var ratingProvider = snapshot3.child(sid).val().Rate;
+              ratingProvider=ratingProvider.toFixed(3);
+              document.getElementById('rate').innerHTML = ratingProvider;
             }
           }
         });
       });
-      });
-          
-        
+    });
+
+
   });
 }
 
-function orderService(){
+function orderService() {
   //var amount=document.getElementById("amount").value;
-  var serviceid=localStorage.getItem("Service_id");
+  var serviceid = localStorage.getItem("Service_id");
   // window.alert(productid);
 
-    var sid=localStorage.getItem("s_id");
-    var uid=localStorage.getItem("u_id");
-    var distnce=localStorage.getItem("distance");
-    var cnt;
-    cnt=parseInt(sid)+parseInt(uid);
-    var reforder=firebase.database().ref('PendingDeal/'+cnt);
-    reforder.once("value")
-.then(function(snapshot) {
-var count=snapshot.numChildren()+1;
-//cnt=parseInt(count);
+  var sid = localStorage.getItem("s_id");
+  var uid = localStorage.getItem("u_id");
+  var distnce = localStorage.getItem("distance");
+  var cnt;
+  cnt = parseInt(sid) + parseInt(uid);
+  var reforder = firebase.database().ref('PendingDeal/' + cnt);
+  reforder.once("value")
+    .then(function (snapshot) {
+      var count = snapshot.numChildren() + 1;
+      //cnt=parseInt(count);
 
 
-var newstr='false';
-window.alert(cnt+' '+newstr+' '+serviceid+' '+sid+' '+uid+' '+distnce);
-firebase.database().ref('PendingDeal/'+cnt+'/'+count).set({
-  OrderID : cnt,
-  Approval : newstr,
-  UserProductId : serviceid,
- SellerID: sid,
- BuyerID : uid,
- Distance : distnce
-});
+      var newstr = 'false';
+      window.alert(cnt + ' ' + newstr + ' ' + serviceid + ' ' + sid + ' ' + uid + ' ' + distnce);
+      firebase.database().ref('PendingDeal/' + cnt + '/' + count).set({
+        OrderID: cnt,
+        Approval: newstr,
+        UserProductId: serviceid,
+        SellerID: sid,
+        BuyerID: uid,
+        Distance: distnce
+      });
 
-});
-
-
-var refpolist=firebase.database().ref('PendingDealList/'+sid);
-    refpolist.once("value")
-.then(function(snapshot) {
-var count=snapshot.numChildren()+1;
-
-firebase.database().ref('PendingDealList/'+sid+'/'+count).set(
- cnt
-   
-);
-
-});
+    });
 
 
-var firesellerOrder=firebase.database().ref('ServiceProviderOrder/'+sid);
-firesellerOrder.once("value")
-.then(function(snapshot) {
-var vcount=snapshot.numChildren()+1;
+  var refpolist = firebase.database().ref('PendingDealList/' + sid);
+  refpolist.once("value")
+    .then(function (snapshot) {
+      var count = snapshot.numChildren() + 1;
+
+      firebase.database().ref('PendingDealList/' + sid + '/' + count).set(
+        cnt
+
+      );
+
+    });
 
 
-//window.alert(vcount);
-firebase.database().ref('ServiceProviderOrder/'+sid+'/'+vcount).set(
-  cnt
-);
+  var firesellerOrder = firebase.database().ref('ServiceProviderOrder/' + sid);
+  firesellerOrder.once("value")
+    .then(function (snapshot) {
+      var vcount = snapshot.numChildren() + 1;
 
-});
+
+      //window.alert(vcount);
+      firebase.database().ref('ServiceProviderOrder/' + sid + '/' + vcount).set(
+        cnt
+      );
+
+    });
 
 }
 
